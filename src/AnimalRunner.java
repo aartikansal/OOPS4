2626470f669be94e8e5c862bd2494e03bb9be0aa
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-19 15:23:21 -0500

    Abstract clas created

---
+++ src/AnimalRunner.java
@@ -0,0 +1,14 @@
+public class AnimalRunner {
+    abstract class Animal{
+        abstract void bark();
+
+    }
+    class  Dog extends Animal{
+        public void bark(){
+            System.out.println("Bow Bow");
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
